175d58b821d16c8f66053f54ebf375faa2baf7a6
STEVENSON <Q5120984@live.tees.ac.uk>
2017-02-08 12:28:17 +0000

    Dumb stuff I was messing with, don't worry about it

---
+++ Assets/Scripts/Units/PlayerManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerManager : UnitManager {
+
+    public GameObject aimIndicator;
+
+    float inputX, inputY;
+    float inputXR, inputYR;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+        inputX = Input.GetAxisRaw("Horizontal");
+        inputY = Input.GetAxisRaw("Vertical");
+        inputXR = Input.GetAxisRaw("HorizontalR");
+        inputYR = Input.GetAxisRaw("VerticalR");
+        aimTowardsLocal(new Vector3(inputXR,0,inputYR));
+        aimIndicator.transform.localPosition = new Vector3(aimDirection.x,0,aimDirection.y); 
+	}
+
+    void FixedUpdate()
+    {
+        Vector3 currentMoveVector = new Vector3(inputX,0,inputY);
+        MoveIn(currentMoveVector);
+    }
+
+}
---
+++ Assets/Scripts/Units/UnitManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class UnitManager : MonoBehaviour {
+
+    public float health;
+    public float maxHealth;
+    public float movementSpeed = 5;
+
+    public float isoAngleOffset = 45;
+
+    protected Vector2 aimDirection = new Vector2(0,0);
+
+    public enum TimeStepType
+    {
+        Fixed,
+        Normal
+    }
+
+    public void MoveIn(Vector3 vector, TimeStepType timeType = TimeStepType.Fixed)
+    {
+        transform.position += Quaternion.AngleAxis(isoAngleOffset, Vector3.up) * vector.normalized * movementSpeed * ((timeType == TimeStepType.Fixed) ? Time.fixedDeltaTime : Time.deltaTime);
+    }
+
+    public void MoveIn(Vector3 vector, float timeStep)
+    {
+        transform.position += vector.normalized * movementSpeed * timeStep;
+    }
+
+    public void aimTowards(Vector3 position)
+    {
+        Vector3 diff = (position - transform.position).normalized;
+        aimDirection = new Vector2(diff.x, diff.z);
+    }
+
+    public void aimTowardsLocal(Vector3 localPosition)
+    {
+        aimDirection = new Vector2(localPosition.x, localPosition.z);
+    }
+
+}
