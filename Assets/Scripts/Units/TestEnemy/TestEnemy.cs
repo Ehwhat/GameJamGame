39458f8cf346cc360d758ccb1e9f5b35c8806461
Joshua Stevenson <JoshuaStevenson97@gmail.com>
2017-03-07 06:14:39 +0000

    Added Seeker logic to an FSM agent
    Badly though, using instance is rather underwelming, looking into better solutions now

--- Assets/Scripts/Units/TestEnemy/TestEnemy.cs
+++ Assets/Scripts/Units/TestEnemy/TestEnemy.cs
@@ -3,12 +3,22 @@ using System.Collections;
 
 public class TestEnemy : AIUnitManager<TestEnemy> {
 
-    public Vector3 patrolPosition;
+    public float minPathUpdateTime = 0.2f;
+    public float pathUpdateMoveThreshold = 0.5f;
 
-	// Use this for initialization
-	void Start () {
+    public Transform target;
+    public float speed = 50;
+    public float turnSpeed = 3;
+    public float turnDst = 5;
+    public float stoppingDst = 10;
+
+    public Path path;
+
+    // Use this for initialization
+    void Start () {
         stateMachine = new FSM<TestEnemy, UnitState<TestEnemy>>("Patrol", this, new TestEnemy_Patrol("Patrol"));
-        patrolPosition = transform.position;
+        if (stateMachine == null)
+            Debug.Log("null");
 	}
 	
 	// Update is called once per frame
