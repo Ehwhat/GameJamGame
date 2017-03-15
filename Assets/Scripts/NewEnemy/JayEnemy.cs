ae34db93aea1e91b672308546eec44636cac587c
Jay Peet <jaytechmedia@googlemail.com>
2017-03-15 14:44:50 +0000

    Connected AI to Pathfinding - Needs Work

--- Assets/Scripts/NewEnemy/JayEnemy.cs
+++ Assets/Scripts/NewEnemy/JayEnemy.cs
@@ -3,6 +3,8 @@ using System.Collections;
 /// <summary>
 /// This is a simple implementation of an Enemy. With patroling, and chasing.
 /// I will move each state into its own function later, so its easier to read.
+/// 
+/// We can stop the enemy from processing when they are far away from the player
 /// </summary>
 public class JayEnemy : MonoBehaviour {
 
@@ -15,6 +17,8 @@ public class JayEnemy : MonoBehaviour {
         ePathTest
     }
 
+    public Renderer rendMesh;
+
     //Speeds for movement.
     public float chaseSpeed = 4;
     public float patrolSpeed = 2;
@@ -22,15 +26,16 @@ public class JayEnemy : MonoBehaviour {
     //Vision Information
     float visionRange = 10;
     Transform visionPoint;
-    Transform[] playerTransforms;
+    public Transform[] playerTransforms;
+    int chaseTarget = 0;
 
     //The enemys rigidbody
     private Rigidbody rb;
 
     //The location of where the Enemy is moving to
-    public Transform target;
+    //public Transform target;
 
-    int currentPoint = 0;
+    public int currentPoint = 0;
     float currentWait = 0;
     //An array of all the Enemys patrol points
     public JayPatrolPoint[] patrolPoints;
@@ -43,6 +48,11 @@ public class JayEnemy : MonoBehaviour {
     bool tempCalc = false;
     Vector3[] chasePath;
     PathRequest pathRequest;
+
+    //Temp node display
+    public GameObject nodePrefab;
+    GameObject[] nodes;
+
     //PathRequestManager pathManager;
     void Start()
     {
@@ -107,35 +117,49 @@ public class JayEnemy : MonoBehaviour {
                 currentWait = 0;
             }
         }
-        else if (currentState == EnemyState.eChasing)
-        {
-            //Run after the players, shoot them etc. Still need to use Pathfinding
-        }
-        else if (currentState == EnemyState.ePathTest && !tempCalc)
-        {
-            currentPoint = 0;
-            GeneratePath(playerTransforms[0].position);
-        }
         else if (currentState == EnemyState.ePatroling)
         {
-           
-                if (currentPoint + 1 < chasePath.Length)
-                {
-                    //Switch to the next point
-                    currentPoint++;
-                }
-                else
-                {
-                    //Back to first point
-                    currentPoint = 0;
-                }
 
-                //Temp Fix
-                Vector3 rot = (chasePath[currentPoint] - rb.position).normalized;
+            if (currentPoint + 1 < chasePath.Length)
+            {
+                //Switch to the next point
+                currentPoint++;
+            }
+            else
+            {
+                //Back to first point
+                currentPoint = 0;
+            }
 
-                rot.y = rb.position.y;
-                Quaternion lookRotation = Quaternion.LookRotation(rot);
-                rb.MoveRotation(Quaternion.Euler(0, lookRotation.eulerAngles.y, 0));
+            //Temp Fix
+            Vector3 rot = (chasePath[currentPoint] - rb.position).normalized;
+
+            rot.y = rb.position.y;
+            Quaternion lookRotation = Quaternion.LookRotation(rot);
+            rb.MoveRotation(Quaternion.Euler(0, lookRotation.eulerAngles.y, 0));
+        }
+        else if (currentState == EnemyState.eChasing && !tempCalc)
+        {
+            currentPoint = 0;
+            GeneratePath(playerTransforms[chaseTarget].position);
+            tempCalc = true;
+            StartCoroutine(UpdatePath());
+        }
+        else if (currentState == EnemyState.eChasing && chasePath != null)
+        {
+            //Check if we are on the current chase Point, then increment.
+            if ((rb.position - chasePath[currentPoint]).magnitude <= 1 && currentPoint + 1 < chasePath.Length)//1 should be the engagement distance
+            {
+                currentPoint++;
+            }
+
+
+            //Temp Fix
+            Vector3 rot = (chasePath[currentPoint] - rb.position).normalized;
+
+            rot.y = rb.position.y;
+            Quaternion lookRotation = Quaternion.LookRotation(rot);
+            rb.MoveRotation(Quaternion.Euler(0, lookRotation.eulerAngles.y, 0));
         }
     }
 
@@ -148,24 +172,22 @@ public class JayEnemy : MonoBehaviour {
         else if (currentState == EnemyState.ePatroling)
         {
             Vector3 distance = (rb.position - patrolPoints[currentPoint].location.position);
-            //To stop upwards movement;
 
             Vector3 direction = distance.normalized;
 
-            //Debug.Log(Mathf.Abs(distance.x));
-
-            /*if (Mathf.Abs(distance.x) < 1)
-            {
-                direction.y = rb.position.y;
-                Quaternion lookRotation = Quaternion.LookRotation(direction);
-                rb.MoveRotation(Quaternion.Euler(0, lookRotation.eulerAngles.y, 0));
-            }*/
             direction.y = 0;
             rb.MovePosition(rb.position - direction * Time.fixedDeltaTime * patrolSpeed);
         }
-        else if (currentState == EnemyState.eChasing)
+        else if (currentState == EnemyState.eChasing && chasePath != null)
         {
             //Run after the players, shoot them etc. Still need to use Pathfinding
+            Vector3 distance = (rb.position - chasePath[currentPoint]);
+
+            Vector3 direction = distance.normalized;
+
+            direction.y = 0;
+            rb.MovePosition(rb.position - direction * Time.fixedDeltaTime * chaseSpeed);
+
         }
         else if (currentState == EnemyState.ePathTest)
         {
@@ -182,8 +204,7 @@ public class JayEnemy : MonoBehaviour {
 
     void Look()
     {
-        
-        RaycastHit hit;
+       
 
 
         //For each players transform, check if they are in range, and within the vision cone.
@@ -191,6 +212,14 @@ public class JayEnemy : MonoBehaviour {
         {
             Vector3 playerRaycast = (playerTransforms[playerIndex].position - visionPoint.position).normalized;
             float visionAngle = Vector3.Dot(visionPoint.forward, playerRaycast);
+
+            //TODO : Need to do a raycast to see if we are seeing the player through a wall as well.
+            if (visionAngle > 0.2f)
+            {
+                rendMesh.material.color = Color.red;
+                chaseTarget = playerIndex;
+                currentState = EnemyState.eChasing;
+            }
             //Debug.Log(visionAngle);
         }
 
@@ -201,16 +230,29 @@ public class JayEnemy : MonoBehaviour {
     {
         pathRequest.pathStart = rb.position;
         pathRequest.pathEnd = _target;
-        System.Action<Vector3[], bool> _callback = new System.Action<Vector3[], bool>(pathCallback);
+        System.Action<Vector3[], bool> _callback = new System.Action<Vector3[], bool>(PathCallback);
         pathRequest.callback = _callback;
 
         PathRequestManager.RequestPath(pathRequest);
     }
 
-    void pathCallback(Vector3[] v, bool b)
+    void PathCallback(Vector3[] v, bool b)
     {
         chasePath = v;
-        currentState = EnemyState.ePathTest;
+        //currentState = EnemyState.ePathTest;
+
+        nodes = new GameObject[v.Length];
+        //Generate display
+        for (int x = 0; x < v.Length; x++)
+        {
+            nodes[x] = Instantiate(nodePrefab, v[x], Quaternion.identity) as GameObject;
+        }
+    }
+
+    void ClearPath()
+    {
+        chasePath = null;
+        chaseTarget = 0;
     }
 
     //For in the future when we have to pass the points in from the terrain generator. 
@@ -218,5 +260,22 @@ public class JayEnemy : MonoBehaviour {
     {
         currentPoint = _currentPoint;
         patrolPoints = _points;
+    }
+
+    IEnumerator UpdatePath()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.1f);
+            yield return new WaitForFixedUpdate();
+
+            for (int x = 0; x < nodes.Length; x++)
+            {
+                Destroy(nodes[x]);
+            }
+
+           // Debug.Log("Update Path");
+            GeneratePath(playerTransforms[chaseTarget].position);
+        }
     }
 }
