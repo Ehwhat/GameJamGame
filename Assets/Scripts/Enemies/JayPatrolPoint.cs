b7c8546bd347567135f34655b4c05a005fd00424
Jay Peet <jaytechmedia@googlemail.com>
2017-05-16 17:40:53 +0100

    Death of The enemy

---
+++ Assets/Scripts/Enemies/JayPatrolPoint.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JayPatrolPoint : MonoBehaviour
+{
+
+    public float waitTime;
+    public Transform location;
+
+    void Start()
+    {
+        location = gameObject.transform;
+    }
+
+}
